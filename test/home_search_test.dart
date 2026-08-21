1aae6b744f8e321a66bcd793cb5b08a9ec24a6b4
redaa4u774201299-crypto <2.8382731e+08+redaa4u774201299-crypto@users.noreply.github.com>
2026-08-21 19:10:44 +0000

    Test home search whitespace and special characters

--- test/home_search_test.dart
+++ test/home_search_test.dart
@@ -20,5 +20,27 @@ void main() {
       expect(uri.path, '/jobs');
       expect(uri.queryParameters, {'q': 'مطور Flutter'});
     });
+
+    test('يرفض إدخالًا مكوّنًا من مسافات وعلامات تبويب وأسطر فقط', () {
+      const criteria = HomeSearchCriteria(query: ' \t\n  ');
+
+      expect(criteria.normalizedQuery, isEmpty);
+      expect(criteria.isEmpty, isTrue);
+      expect(criteria.jobsPath, isNull);
+    });
+
+    test('يحافظ على الرموز الخاصة عند ترميز رابط البحث وفكّه', () {
+      const query = '  C++ & QA/اختبار #1؟  ';
+      const criteria = HomeSearchCriteria(query: query);
+
+      final path = criteria.jobsPath;
+
+      expect(path, isNotNull);
+      expect(path, contains('%2B%2B'));
+      expect(path, contains('%26'));
+      final uri = Uri.parse(path!);
+      expect(uri.path, '/jobs');
+      expect(uri.queryParameters, {'q': 'C++ & QA/اختبار #1؟'});
+    });
   });
 }
